487b4b9a2880c263895dfaf7a44a15bb59b8b20a
Benjamin Tan Wei Hao <benjamintanweihao@gmail.com>
2015-05-28 00:26:23 +0800

    First working version.

--- lib/thy_supervisor.ex
+++ lib/thy_supervisor.ex
@@ -8,24 +8,36 @@ defmodule ThySupervisor do
     spawn_link(__MODULE__, :init, [child_spec_list])
   end
 
+  # TODO: Should this be synchronous?
   def start_child(supervisor, {mod, fun, args}) do
     supervisor |> send({:start_child, {mod, fun, args}})
-  end
-
-  def restart_child(supervisor, pid) when is_pid(pid) do
-    supervisor |> send({:restart_child, pid})
   end
 
   def terminate_child(supervisor, pid) when is_pid(pid) do
     supervisor |> send({:terminate_child, pid})
   end
 
-  def which_children(supervisor) do
-    supervisor |> send({:which_children, self})
-  end
-
   def count_children(supervisor) do
     supervisor |> send({:count_children, self})
+    receive do
+      {:ok, count} -> count
+       _ -> :ok
+    end
+  end
+
+  def which_children(supervisor) do
+    supervisor |> send({:which_children, self})
+    receive do
+      {:ok, children} -> children
+       _ -> :ok
+    end
+  end
+
+  def stop(supervisor) do
+    supervisor |> send({:stop, self})
+    receive do
+      :ok -> :ok
+    end
   end
 
   #####################
@@ -37,13 +49,18 @@ defmodule ThySupervisor do
     child_spec_list |> start_children |> loop
   end
 
+  def start_child({mod, fun, args}) do
+    pid = apply(mod, fun, args)
+    HashDict.new |> HashDict.put(pid, {mod, fun, args})
+  end
+
   def start_children(child_spec_list) do
     start_children(child_spec_list, HashDict.new)
   end
 
   # NOTE: We assume that the child calls start_link, otherwise there's no point.
-  def start_children([{mod, fun, args}|rest], state) do
-    new_state = HashDict.put(state, start_child({mod, fun, args}))
+  def start_children([child_spec|rest], state) do
+    new_state = state |> HashDict.merge(start_child(child_spec))
     start_children(rest, new_state)
   end
 
@@ -51,47 +68,57 @@ defmodule ThySupervisor do
     state
   end
 
-  def start_child({mod, fun, args}) do
-    {:ok, pid} = apply(mod, fun, args)
-    {pid, {mod, fun, args}}
-  end
-
   def terminate_child(pid) do
-    Process.unlink(pid)
     Process.exit(pid, :kill)
     pid
   end
 
+  def terminate_children([]) do
+    :ok
+  end
+
+  def terminate_children([child|children]) do
+    terminate_child(child)
+    terminate_children(children)
+  end
+
   def loop(state) do
     receive do
-      {:start_child, {mod, fun, args}} ->
-        new_state = state |> HashDict.put(start_child({mod, args, fun}))
 
+      {:start_child, child_spec} ->
+        new_state = state |> HashDict.merge(start_child(child_spec))
         loop(new_state)
 
       {:restart_child, old_pid} ->
-        {:ok, child_spec} = HashDict.fetch(state, old_pid)
+        case HashDict.fetch(state, old_pid) do
+          {:ok, child_spec} ->
 
-        new_state = state
-                      |> HashDict.delete(terminate_child(old_pid))
-                      |> HashDict.put(start_child(child_spec))
+          new_state = state
+                        |> HashDict.delete(terminate_child(old_pid))
+                        |> HashDict.merge(start_child(child_spec))
 
-        loop(new_state)
+          loop(new_state)
+
+          _ ->
+            loop(state)
+
+        end
 
       {:terminate_child, pid} ->
-        new_state = HashDict.delete(terminate_child(pid))
-
+        new_state = state |> HashDict.delete(terminate_child(pid))
         loop(new_state)
 
-      {:which_children, from} ->
-        send(from, state)
-
-        loop(state)
-
       {:count_children, from} ->
-        send(from, state |> HashDict.count)
-
+        send(from, {:ok, state |> HashDict.size})
         loop(state)
+
+      {:which_children, from} ->
+        send(from, {:ok, state})
+        loop(state)
+
+      {:stop, from} ->
+        terminate_children(state |> HashDict.keys)
+        send(from, :ok)
 
       {:EXIT, from, :normal} ->
         new_state = HashDict.delete(state, from)
@@ -99,13 +126,17 @@ defmodule ThySupervisor do
         loop(new_state)
 
       {:EXIT, from, _reason} ->
-        {:ok, child_spec} = HashDict.fetch(state, from)
-        Process.unlink(from)
-        new_state = state
-                      |> HashDict.delete(from)
-                      |> HashDict.put(start_child(child_spec))
+        case HashDict.fetch(state, from) do
+          {:ok, child_spec} ->
+            new_state = state
+                          |> HashDict.delete(from)
+                          |> HashDict.merge(start_child(child_spec))
 
-        loop(new_state)
+            loop(new_state)
+
+          _ ->
+            loop(state)
+        end
 
       _ ->
         loop(state)
---
+++ lib/thy_worker.ex
@@ -0,0 +1,15 @@
+defmodule ThyWorker do
+  def start_link do
+    spawn_link(fn -> loop end)
+  end
+
+  def loop do
+    receive do
+      :stop -> :ok
+
+      msg ->
+        IO.inspect msg
+        loop
+    end
+  end
+end
